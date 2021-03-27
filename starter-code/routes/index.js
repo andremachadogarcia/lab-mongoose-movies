87274cd27b08a2887b2d1ab728ce654ef64ba90b
“AndreGarcia <andre.m.garcia@icloud.com>
2021-03-26 23:37:17 -0300

    Update index.js

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const Celebrity = require('../models/Celebrity');
+
 const router  = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/celebrities', async (req, res, next) => {
+  try {
+    const celebrities = await Celebrity.find();
+
+    res.render('celebrities', { celebrities });
+  } catch (error) {
+    next(error)
+
+    return error;
+
+  }
+})
 
 module.exports = router;
